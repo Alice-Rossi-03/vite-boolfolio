857d25938f7b47d8b2c39c80519177fea9731361
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-04-04 13:19:19 +0200

    feat (all) /card-structure /add-component-MainApp

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import MainApp from './components/MainApp.vue';
+
+export default{
+  components:{
+    MainApp
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <header>
+      <h1 class="my-3 text-center fw-bolder">Project Cards</h1>
+      <MainApp/>
+   </header>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,37 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'MainApp',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div class="pt-4 container d-flex flex-wrap justify-content-center gap-4">
+
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped></style>
